9fa6f199d49b9e8a8251fc71ac2703bad7226acb
Tomas <nahuel568178@gmail.com>
2023-09-29 19:14:06 -0300

    collaborative_filtering model

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from fastapi import FastAPI, HTTPException
 import pandas as pd
 
+from tensorflow.python.ops.numpy_ops import np_config
+np_config.enable_numpy_behavior()
+from keras.models import load_model
+
 playtimegenre_df = pd.read_parquet('ApiDatasets/playtimegenre.parquet')
 userforgenre_df = pd.read_parquet('ApiDatasets/userforgenre.parquet')
 usersrecommend_df = pd.read_parquet('ApiDatasets/usersrecommend.parquet')
 usersnotrecommend_df = pd.read_parquet('ApiDatasets/usersnotrecommend.parquet')
+sentimentanalysis_df = pd.read_parquet('ApiDatasets/sentimentanalysis.parquet')
+model = load_model('Models/collaborative_filtering')
+
 
 app = FastAPI()
 
-@app.get('/PlayTimeGente/{genre}')
+@app.get('/PlayTimeGenre/{genre}')
 async def playtimegenre(genre:str):
     genre = genre.title()
 
@@ -51,10 +58,30 @@ async def usersrecommend(year:int):
 async def usersnotrecommend(year:int):
     df = usersnotrecommend_df[usersnotrecommend_df['year'] == year]
     position_game = df[['position', 'title']].to_numpy().tolist()
-    
-    try:
+
+    try: 
         position_game[0]
     except IndexError: #Nonexistent year
         raise HTTPException(status_code=404, detail=f"The year {year} doesn't exists in our database")
 
     return [{position: f"{title}" for position, title in position_game}]
+
+@app.get('/SentimentAnalysis/{year}')
+async def sentimentanalysis(year:int):
+    labels = {0:'Negative', 1:'Neutral', 2:'Positive'}
+    df = sentimentanalysis_df[sentimentanalysis_df['year'] == year]
+    df.reset_index(drop=True, inplace=True)
+
+    try:
+        df.loc[0]
+    except (ValueError, KeyError): #Nonexistent year
+        raise HTTPException(status_code=404, detail=f"The year {year} doesn't exists in our database")
+    
+    return {labels[key]:value for key, value in zip(df['sentiment_analysis'].tolist() ,df['count'].tolist())}
+
+@app.get('/CollaborativeFiltering/{user_id}')
+async def collaborativefiltering(user_id:str):
+
+    scores, titles = model([user_id])
+    return{f"Top 5 recommendations for user {user_id}: {[title.decode('utf-8').split(',')[0] for title in titles[0,:5].tolist()]}"}
+
